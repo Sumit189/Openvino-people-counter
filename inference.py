aca318fb562a183311656092822a57f3d39ccaf2
Sumit <sumit.18.paul@gmail.com>
2020-05-23 01:54:54 +0530

    Update inference.py

--- inference.py
+++ inference.py
@@ -29,48 +29,67 @@ from openvino.inference_engine import IENetwork, IECore
 
 
 class Network:
-    """
-    Load and configure inference plugins for the specified target devices 
-    and performs synchronous and asynchronous modes for the specified infer requests.
-    """
-
     def __init__(self):
-        ### TODO: Initialize any class variables desired ###
-        self.network = None
-        self.core = None
+        self.net = None
+        ie = None
         self.input_blob = None
         self.out_blob = None
-        self.exec_network = None
-        self.infer_request = None
+        self.net_plugin = None
+        self.infer_request_handle = None
 
-    def load_model(self, model_xml, device='CPU', cpu_extension=None):
-        ### TODO: Load the model ###
-        self.network = IENetwork(model=model_xml, weights=model_xml[:-3] + 'bin')
+    def load_model(self, model, device, input_size, output_size, num_requests, extn=None):
+        model_xml = model
+        model_bin = os.path.splitext(model_xml)[0] + ".bin"
         
-        # Initialise Core
-        self.core = IECore()
-        if cpu_extension and "CPU" in device:
-            self.core.add_extension(cpu_extension, device)
+        log.info("Creating Inference Engine...")
+        ie = IECore()
+        if extn and 'CPU' in device:
+            ie.add_extension(extn, "CPU")
+                   
+        # Read IR
+        log.info("Reading IR...")
+        self.net = IENetwork(model=model_xml, weights=model_bin)
             
-        # attach network to core
-        self.exec_network = self.core.load_network(self.network, device)
-        
-        self.input_blob = next(iter(self.network.inputs))
-        self.output_blob = next(iter(self.network.outputs))
+        if "CPU" in device:
+            supported_layers = ie.query_network(self.net, "CPU")
+            not_supported_layers = [l for l in self.net.layers.keys() if l not in supported_layers]
+            if len(not_supported_layers) != 0:
+                sys.exit(1)
+            
+        if num_requests == 0:
+            self.net_plugin = ie.load_network(network=self.net, device_name=device)
+        else:
+            self.net_plugin = ie.load_network(network=self.net, device_name=device, num_requests=num_requests)
+
+        self.input_blob = next(iter(self.net.inputs))
+        self.out_blob = next(iter(self.net.outputs))
+        assert len(self.net.inputs.keys()) == input_size, \
+            "Supports only {} input topologies".format(len(self.net.inputs))
+        assert len(self.net.outputs) == output_size, \
+            "Supports only {} output topologies".format(len(self.net.outputs))
+
+        return ie, self.get_input_shape()
+
 
     def get_input_shape(self):
-        ### TODO: Return the shape of the input layer ###
-        return self.network.inputs[self.input_blob].shape
+        return self.net.inputs[self.input_blob].shape
 
-    def init_async_infer(self,image):
-        ### TODO: Start an asynchronous request ###
-        ### TODO: Return any necessary information ###
-        self.exec_network.start_async(request_id=0, inputs={self.input_blob: image})
+    def exec_net(self, request_id, frame):
+        self.infer_request_handle = self.net_plugin.start_async(request_id=request_id, inputs={self.input_blob: frame})
+        return self.net_plugin
 
-    def wait(self):
-        ### TODO: Wait for the request to be complete. ###
-        wait_=self.exec_network.requests[0].wait(-1)
-        return wait_
 
-    def get_output(self):
-        return self.exec_network.requests[0].outputs[self.output_blob]
+    def wait(self, request_id):
+        infer_status = self.net_plugin.requests[request_id].wait(-1)
+        return infer_status
+
+    def get_output(self, request_id, output=None):
+        if output:
+            res = self.infer_request_handle.outputs[output]
+        else:
+            res = self.net_plugin.requests[request_id].outputs[self.out_blob]
+        return res
+
+    def clean(self):
+        del self.net_plugin
+        del self.net
